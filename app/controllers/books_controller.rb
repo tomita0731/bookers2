b7535fe283ac64bef65a4e0e93a9233cc032aa57
EC2 Default User <ec2-user@ip-172-31-13-190.ap-northeast-1.compute.internal>
2024-10-16 03:13:57 +0000

    [Add]model.database.controller.viewファイル

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
