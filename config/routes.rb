24e9eff9660fef5d910dedc2434dae4e77b02c51
tomita0731 <175428025+tomita0731@users.noreply.github.com>
2024-10-16 09:24:07 +0000

    [Add] header途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'homes/top'
+  root to:"homes#top"
   get 'homes/about'
   devise_for :users
-  
   resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
